3e2e42399220e3a2d1aa82b56a4a8713a89e8b5f
Allison Moss <allisonwmoss@icloud.com>
2021-08-20 10:42:12 -0400

    Added basic structure and junimo divs to HTML. Added styling so that all junimo divs have a corresponding background image. All junimo divs have jump animation on hover.

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('connected')
+//PSEUDOCODE / PLANNING
+
+//MVP
+
+// Offer to explain the game to me before playing.
+    //This will use a modal box that opens when you click a button and hides when you click out of it. 
+// Show me what the correct pattern is that I need to match, using animation of the Junimos to indicate the colors of the pattern.
+// Animate each Junimo when I click or hover over it, so I know which one I'm targeting. 
+// Allow me to restart the game at any time.
+// Tell me when I get the pattern correct, and track my score in a way that I can see. 
+// Inform me that I have lost if I get the pattern incorrect. 
+// Inform me when I win the game.
+
+//STRETCH/IF TIME ALLOWS
+
+// Use a modal to have the game rules appear in a box, which I can then click out of to hide.
+// Show me a cute animation of all the junimos jumping up and down at random when I win. 
+// Give me an option of how many rounds I'd like to play, like Easy, Medium, and Hard modes, so that I can ease into the game or really put my skills to the test. 
+// Require me to repeat the given pattern within a certain amount of time, and show me the time I have left as it ticks down. 
